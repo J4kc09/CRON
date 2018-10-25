b09786a91e8c9a7c9e5cbd7c356fc0c05e83e4e8
J4kc09 <33621408+J4kc09@users.noreply.github.com>
2018-10-25 21:08:49 +0200

    Update Cron-carrascocarmona-practica2-7.sh

--- Cron-carrascocarmona-practica2-7.sh
+++ Cron-carrascocarmona-practica2-7.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 lastweek=`date +%V-%d-%m-%Y`
 
-cat /root/logs/practicacron.txt | grep $lastweek | mailx -s "Logs de la practica de cron " "jandrokc@gmail.com" < /root/logs/practicacron.txt && echo "`date` Logs practicacron.txt enviados" >> /root/logs/practicacron.txt || echo "`date` No se han enviado lso logs practicacron.txt" >> /root/logs/practicacron.txt
+cat /root/logs/practicacron.txt | grep $lastweek | mailx -s "Logs de la practica de cron " "jandrokc@gmail.com" && echo "`date` Logs practicacron.txt enviados" >> /root/logs/practicacron.txt || echo "`date` No se han enviado lso logs practicacron.txt" >> /root/logs/practicacron.txt
